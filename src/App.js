7b6c30cb95a011924edb33a64e6bd7719e9d2ad2
Arjita <arjita461@gmail.com>
2023-07-10 22:42:24 +0530

    Main Branch commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const weekDays = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+  const noOfProducts = 10
+
+
+  const handleChange=(e)=>{
+    var quantity = Number(e.target.value)
+    if(isNaN(quantity)){
+      document.getElementById('quantInput').classList.add('quantity')
+      document.getElementById('errorDiv').style.display='block'
+    }
+    else{
+      document.getElementById('quantInput').classList.remove('quantity')
+      document.getElementById('errorDiv').style.display='none'
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Container'>
+      <div className='Inputs'>
+        <div className="dropdown">
+          <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              --Select Product--
+          </button>
+          <ul className="dropdown-menu">
+            {[...Array(noOfProducts)].map((e,i)=><li><a className='dropdown-item' key={i}>Product#{i+1}</a></li>)}
+          </ul>
+        </div>
+        <div className="dropdown">
+          <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              --Select Week Day--
+          </button>
+          <ul className="dropdown-menu">
+            {weekDays.map((ele,i)=> <li key={i}><a className="dropdown-item">{ele}</a></li>)}
+          </ul>
+        </div>
+        <input placeholder='Quantity'  id='quantInput' onChange={handleChange}/>
+        <span id='errorDiv' className='error'>Quantity should be  natural number only</span>
+        <button className='btn btn-primary'>Add</button>
+      </div>
+      <div className='RightDiv'>
+        <div>
+          
+        </div>
+        <button className='btn btn-primary'>Finish</button>
+      </div>
     </div>
   );
 }
